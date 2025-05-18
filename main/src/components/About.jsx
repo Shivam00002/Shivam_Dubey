f4137cd1dfcadb1f54c33a5ca14bef632dbfa03a
Shivam00002 <dubey02shiv@gmail.com>
2025-05-18 13:39:25 +0530

    fixed

--- main/src/components/About.jsx
+++ main/src/components/About.jsx
@@ -182,86 +182,86 @@ const About = () => {
                     label: "Birth Details",
                     value: "7th September, 1999 at 4:40 AM",
                   },
-                  {
-                    icon: (
-                      <svg
-                        className="w-5 h-5 text-indigo-500 dark:text-indigo-300"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                        ></path>
-                      </svg>
-                    ),
-                    label: "Gotra",
-                    value: "Bharadwaj",
-                  },
-                  {
-                    icon: (
-                      <svg
-                        className="w-5 h-5 text-indigo-500 dark:text-indigo-300"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                        ></path>
-                      </svg>
-                    ),
-                    label: "Age",
-                    value: "25 years",
-                  },
-                  {
-                    icon: (
-                      <svg
-                        className="w-5 h-5 text-indigo-500 dark:text-indigo-300"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
-                        ></path>
-                      </svg>
-                    ),
-                    label: "Height",
-                    value: "5'10\"",
-                  },
-                  {
-                    icon: (
-                      <svg
-                        className="w-5 h-5 text-indigo-500 dark:text-indigo-300"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-                        ></path>
-                      </svg>
-                    ),
-                    label: "Complexion",
-                    value: "Fair",
-                  },
+                  // {
+                  //   icon: (
+                  //     <svg
+                  //       className="w-5 h-5 text-indigo-500 dark:text-indigo-300"
+                  //       fill="none"
+                  //       stroke="currentColor"
+                  //       viewBox="0 0 24 24"
+                  //       xmlns="http://www.w3.org/2000/svg"
+                  //     >
+                  //       <path
+                  //         strokeLinecap="round"
+                  //         strokeLinejoin="round"
+                  //         strokeWidth="2"
+                  //         d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                  //       ></path>
+                  //     </svg>
+                  //   ),
+                  //   label: "Gotra",
+                  //   value: "Bharadwaj",
+                  // },
+                  // {
+                  //   icon: (
+                  //     <svg
+                  //       className="w-5 h-5 text-indigo-500 dark:text-indigo-300"
+                  //       fill="none"
+                  //       stroke="currentColor"
+                  //       viewBox="0 0 24 24"
+                  //       xmlns="http://www.w3.org/2000/svg"
+                  //     >
+                  //       <path
+                  //         strokeLinecap="round"
+                  //         strokeLinejoin="round"
+                  //         strokeWidth="2"
+                  //         d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                  //       ></path>
+                  //     </svg>
+                  //   ),
+                  //   label: "Age",
+                  //   value: "25 years",
+                  // },
+                  // {
+                  //   icon: (
+                  //     <svg
+                  //       className="w-5 h-5 text-indigo-500 dark:text-indigo-300"
+                  //       fill="none"
+                  //       stroke="currentColor"
+                  //       viewBox="0 0 24 24"
+                  //       xmlns="http://www.w3.org/2000/svg"
+                  //     >
+                  //       <path
+                  //         strokeLinecap="round"
+                  //         strokeLinejoin="round"
+                  //         strokeWidth="2"
+                  //         d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
+                  //       ></path>
+                  //     </svg>
+                  //   ),
+                  //   label: "Height",
+                  //   value: "5'10\"",
+                  // },
+                  // {
+                  //   icon: (
+                  //     <svg
+                  //       className="w-5 h-5 text-indigo-500 dark:text-indigo-300"
+                  //       fill="none"
+                  //       stroke="currentColor"
+                  //       viewBox="0 0 24 24"
+                  //       xmlns="http://www.w3.org/2000/svg"
+                  //     >
+                  //       <path
+                  //         strokeLinecap="round"
+                  //         strokeLinejoin="round"
+                  //         strokeWidth="2"
+                  //         d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                  //       ></path>
+                  //     </svg>
+                  //   ),
+                  //   label: "Complexion",
+                  //   value: "Fair",
+                  // },
                 ].map((item, index) => (
                   <motion.div
                     key={index}
